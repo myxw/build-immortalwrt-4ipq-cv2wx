46c67c6cba59a68b07974fcae2a585c46ffbf186
xinruoyusixian <xinruoyusixian@163.com>
2025-10-04 23:41:55 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -45,46 +45,16 @@ UPDATE_PACKAGE() {
 # UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master" "" "luci-app-appfilter oaf" 这样会把原有的open-app-filter，luci-app-appfilter，oaf相关组件删除，不会出现coremark错误。
 
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
-UPDATE_PACKAGE "argon" "chingjyu/luci-theme-argon" "main"
-UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
-UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
-
-UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+#UPDATE_PACKAGE "argon" "chingjyu/luci-theme-argon" "main"
+#UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 
 
 
-#UPDATE_PACKAGE "argon" "xinruoyusixian/alan-package" "main" "pkg" #自定义主题插件
-
-#流量监控插件
-UPDATE_PACKAGE "luci-app-bandix" "timsaya/luci-app-bandix" "main" 
-UPDATE_PACKAGE "bandix" "timsaya/openwrt-bandix" "main"
-
-UPDATE_PACKAGE "luci-app-taskplan" "xinruoyusixian/alan-package" "main" "pkg"   
-UPDATE_PACKAGE "luci-app-nft-timecontrol" "xinruoyusixian/alan-package" "main" "pkg"
-
-UPDATE_PACKAGE "luci-app-eqosplus" "xinruoyusixian/alan-package" "main"  "pkg"
-
-UPDATE_PACKAGE "luci-app-watchdog" "xinruoyusixian/alan-package" "main" "pkg"
-UPDATE_PACKAGE "watchdog" "xinruoyusixian/alan-package" "main" "pkg"
-
-UPDATE_PACKAGE "luci-app-unishare" "xinruoyusixian/alan-package" "main" "pkg"
-UPDATE_PACKAGE "luci-app-ddnsto" "xinruoyusixian/alan-package" "main" "pkg"
-UPDATE_PACKAGE "ddnsto" "xinruoyusixian/alan-package" "main" "pkg"
-UPDATE_PACKAGE "unishare" "xinruoyusixian/alan-package" "main" "pkg"
-UPDATE_PACKAGE "webdav2" "xinruoyusixian/alan-package" "main" "pkg"
-
-UPDATE_PACKAGE "luci-app-wrtbwmon" "xinruoyusixian/alan-package" "main" "pkg"
-
-UPDATE_PACKAGE "wrtbwmon" "xinruoyusixian/alan-package" "main" "pkg"
-UPDATE_PACKAGE "wrtbwmon" "xinruoyusixian/alan-package" "main" "pkg"
-
-UPDATE_PACKAGE "quickfile" "xinruoyusixian/alan-package" "main" "pkg"
-UPDATE_PACKAGE "luci-app-quickfile" "xinruoyusixian/alan-package" "main" "pkg"
 
 
-UPDATE_PACKAGE "luci-app-adguardhome" "xinruoyusixian/alan-package" "main" "pkg"
-UPDATE_PACKAGE "adguardhome" "xinruoyusixian/alan-package" "main" "pkg"
+
+
 
 #更新软件包版本
 UPDATE_VERSION() {
@@ -128,5 +98,4 @@ UPDATE_VERSION() {
 }
 
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
-UPDATE_VERSION "sing-box"
 UPDATE_VERSION "tailscale"
